28211934634f2f6c43bf639c819f01feb449dd05
Thomas Nishiduka <thomas.takao@pixter.com>
2022-03-30 01:03:52 -0300

    Atualizado Readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# motor-shield
-Projeto arduino com motor shield contro, usando a tecnologia bluetooth
+# Projeto Arduino de carrinho
+Um projeto feito no curso técnico em 2015, feito para entender o funcionamento do Arduíno com placas adicionais(os shields).
+
+
+Neste projeto foi utilizado: 
+- Arduino uno;
+- Sensor ultrassônico HC-SR04;
+- Módulo bluetooth HC-05;
+- Motor Shield L293D Driver Ponte H para Arduino;
+- 4 Motores DC 3-6V com Caixa de Redução e Eixo Duplo.
+
+Para a comunição bluetooth, foi utilizado um smartphone com o app de controle remoto. Os comandos foram remapeados no aplicativo para que o programa reconheça a ordem para executar.
+
+<img src="./image4.jpg"/>
--- arduino.cpp
+++ arduino.cpp
@@ -4,7 +4,7 @@
 int trigPin(24);//<- seta a porta do arduino, no caso uma analogica do arduino mega
 int echoPin(22);
 
-AF_DCMotor motor1(1);//o "motor1" é uma variavel e o número de parâmetro o motor correspondente no motor shield
+AF_DCMotor motor1(1); // Funcao diz a biblioteca quais os pins dos motores
 AF_DCMotor motor2(2);
 AF_DCMotor motor3(3);
 AF_DCMotor motor4(4);
@@ -20,9 +20,9 @@ void loop()
 {
   int velocidade = 130;
   int cont = 1;
-   int a = Serial.read();
+	int a = Serial.read();
 
-if(a != -1){
+	if(a != -1){
 
 		if(a == 'w') {
 
@@ -45,110 +45,111 @@ if(a != -1){
 			duration = pulseIn (echoPin, HIGH);
 			distance = (duration/2) / 29.1;
 
-				while(cont == 1){
+			while( cont == 1 ) {
+				digitalWrite (trigPin, LOW);
+				delayMicroseconds(2);
+				digitalWrite (trigPin, HIGH);
+				delayMicroseconds (10);
+				digitalWrite(trigPin, LOW);
+				duration = pulseIn (echoPin, HIGH);
+				distance = (duration/2) / 29.1;
 
-					digitalWrite (trigPin, LOW);
-					delayMicroseconds(2);
-					digitalWrite (trigPin, HIGH);
-					delayMicroseconds (10);
-					digitalWrite(trigPin, LOW);
-					duration = pulseIn (echoPin, HIGH);
-					distance = (duration/2) / 29.1;
-    				if((a == 's') || (a == 'q') || (a == 'd') || (a == 'a')){ cont = 2;}
-    				if(distance <30){
+				if(
+					(a == 's') || 
+					(a == 'q') || 
+					(a == 'd') || 
+					(a == 'a')
+				) { 
+					cont = 2;
+				}
 
-								motor1.setSpeed(0);
-								motor2.setSpeed(0);
-								motor3.setSpeed(0);
-								motor4.setSpeed(0);
+				if(distance < 30){
+					motor1.setSpeed(0);
+					motor2.setSpeed(0);
+					motor3.setSpeed(0);
+					motor4.setSpeed(0);
 
-								motor1.run(RELEASE);
-								motor2.run(RELEASE);
-								motor3.run(RELEASE);
-								motor4.run(RELEASE);
-						}
-            else{
-              	motor1.setSpeed(velocidade);
-								motor2.setSpeed(velocidade);
-								motor3.setSpeed(velocidade);
-								motor4.setSpeed(velocidade);
+					motor1.run(RELEASE);
+					motor2.run(RELEASE);
+					motor3.run(RELEASE);
+					motor4.run(RELEASE);
+				} else {
+					motor1.setSpeed(velocidade);
+					motor2.setSpeed(velocidade);
+					motor3.setSpeed(velocidade);
+					motor4.setSpeed(velocidade);
 
-								motor1.run(FORWARD);
-								motor2.run(FORWARD);
-								motor3.run(FORWARD);
-								motor4.run(FORWARD);
-
-            }
-		  }
+					motor1.run(FORWARD);
+					motor2.run(FORWARD);
+					motor3.run(FORWARD);
+					motor4.run(FORWARD);
+				}
+			}
 	}
 
-
   if(a == 's') {
+		Serial.println("Indo para trás");
 
-			Serial.println("Indo para trás");//desliga os motores
-
-			motor1.setSpeed(velocidade); //liga os motores
-			motor1.run(BACKWARD);
-			motor2.setSpeed(velocidade);
-			motor2.run(BACKWARD);
-			motor3.setSpeed(velocidade); //liga os motores
-			motor3.run(BACKWARD);
-			motor4.setSpeed(velocidade);
-			motor4.run(BACKWARD);
-}
-
+		motor1.setSpeed(velocidade); //liga os motores
+		motor1.run(BACKWARD);
+		motor2.setSpeed(velocidade);
+		motor2.run(BACKWARD);
+		motor3.setSpeed(velocidade); //liga os motores
+		motor3.run(BACKWARD);
+		motor4.setSpeed(velocidade);
+		motor4.run(BACKWARD);
+	}
 
 	if(a == 'd'){
+		Serial.println("Girando para direita");
+		motor1.setSpeed(150);
+		motor1.run(FORWARD);
+		motor2.setSpeed(150);
+		motor2.run(FORWARD);
+		motor3.setSpeed(150);
+		motor3.run(BACKWARD);
+		motor4.setSpeed(150);
+		motor4.run(BACKWARD) ;
 
-			Serial.println("Girando para direita");
-			motor1.setSpeed(150);
-			motor1.run(FORWARD);
-			motor2.setSpeed(150);
-			motor2.run(FORWARD);
-			motor3.setSpeed(150);
-			motor3.run(BACKWARD);
-			motor4.setSpeed(150);
-			motor4.run(BACKWARD) ;
+		delay(500);
 
-			delay(500);
-
-			motor1.run(RELEASE);
-			motor2.run(RELEASE);
-			motor3.run(RELEASE);
-			motor4.run(RELEASE);
- }
+		motor1.run(RELEASE);
+		motor2.run(RELEASE);
+		motor3.run(RELEASE);
+		motor4.run(RELEASE);
+ 	}
 
 	if(a == 'a'){
-			Serial.println("Girando para esquerda");
+		Serial.println("Girando para esquerda");
 
-			motor1.setSpeed(150);
-			motor1.run(BACKWARD);
-			motor2.setSpeed(150);
-			motor2.run(BACKWARD);
-			motor3.setSpeed(150);
-			motor3.run(FORWARD);
-			motor4.setSpeed(150);
-			motor4.run(FORWARD) ;
+		motor1.setSpeed(150);
+		motor1.run(BACKWARD);
+		motor2.setSpeed(150);
+		motor2.run(BACKWARD);
+		motor3.setSpeed(150);
+		motor3.run(FORWARD);
+		motor4.setSpeed(150);
+		motor4.run(FORWARD) ;
 
-			delay(500);
+		delay(500);
 
-			motor1.run(RELEASE);
-			motor2.run(RELEASE);
-			motor3.run(RELEASE);
-			motor4.run(RELEASE);
- }
+		motor1.run(RELEASE);
+		motor2.run(RELEASE);
+		motor3.run(RELEASE);
+		motor4.run(RELEASE);
+ 	}
 
 	if(a == 'q'){
-			Serial.println("parando");
+		Serial.println("parando");
 
-			motor1.setSpeed(0);
-			motor2.setSpeed(0);
-			motor3.setSpeed(0);
-			motor4.setSpeed(0);
-			motor1.run(RELEASE);
-			motor2.run(RELEASE);
-			motor3.run(RELEASE);
-			motor4.run(RELEASE);
+		motor1.setSpeed(0);
+		motor2.setSpeed(0);
+		motor3.setSpeed(0);
+		motor4.setSpeed(0);
+		motor1.run(RELEASE);
+		motor2.run(RELEASE);
+		motor3.run(RELEASE);
+		motor4.run(RELEASE);
 	}
  }
 }
